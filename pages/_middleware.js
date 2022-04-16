ddf36e93a58c1ced134aa35a87e79f21a1f5da97
Antonio94709 <exilhekkiekassongo6@gmail.com>
2022-04-17 00:33:49 +0200

    Eigth commit

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -11,7 +11,7 @@ export async function middleware(req) {
     }
     // redirect from login
     if (!token && pathname !== '/login') {
-        return NextResponse.redirect('http://localhost:3000/login')
+        return NextResponse.redirect('https://spotify-3-alpha.vercel.app/login')
     }
 
 }
